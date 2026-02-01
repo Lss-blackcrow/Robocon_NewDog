0acc5a8b4fd89f034d4ff1b12b858d86402069fc
lihongjie <lhj1001tcz@163.com>
2026-02-01 14:38:07 +0800

    1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "motors.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
